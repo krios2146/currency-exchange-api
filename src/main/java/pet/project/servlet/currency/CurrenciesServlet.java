91e897023e519c247678fe8fca206ef05ad74fac
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-15 19:38:53 +0700

    feat: implement `doGet` method in `CurrenciesServlet`

--- src/main/java/pet/project/servlet/currency/CurrenciesServlet.java
+++ src/main/java/pet/project/servlet/currency/CurrenciesServlet.java
@@ -1,18 +1,31 @@
 package pet.project.servlet.currency;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pet.project.Utils;
+import pet.project.model.Currency;
+import pet.project.repository.CurrencyRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "CurrenciesServlet", urlPatterns = "/currencies")
 public class CurrenciesServlet extends HttpServlet {
+    private final CurrencyRepository currencyRepository = new CurrencyRepository(Utils.getConfiguredDataSource());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        List<Currency> currencyList = currencyRepository.findAll();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(resp.getWriter(), currencyList);
     }
 
     @Override
