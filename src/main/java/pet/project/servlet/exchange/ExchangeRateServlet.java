184c2074e6606e5f4dab00e7effbfdd91108eb13
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-16 19:36:29 +0700

    feat: implement doGet method in ExchangeRateServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeRateServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeRateServlet.java
@@ -1,14 +1,21 @@
 package pet.project.servlet.exchange;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pet.project.Utils;
+import pet.project.model.ExchangeRate;
+import pet.project.repository.ExchangeRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
-@WebServlet(name = "ExchangeRateServlet", urlPatterns = "/exchangeRate/{param}")
+@WebServlet(name = "ExchangeRateServlet", urlPatterns = "/exchangeRate/*")
 public class ExchangeRateServlet extends HttpServlet {
+    private final ExchangeRepository exchangeRepository = new ExchangeRepository(Utils.getConfiguredDataSource());
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,7 +28,14 @@ public class ExchangeRateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String codes = req.getPathInfo().replaceAll("/", "").toUpperCase();
+        String baseCurrencyCode = codes.substring(0, 3);
+        String targetCurrencyCode = codes.substring(3);
+
+        Optional<ExchangeRate> exchangeRate = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(resp.getWriter(), exchangeRate.get());
     }
 
     protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
