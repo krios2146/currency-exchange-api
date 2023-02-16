6d2285b04bdc5faf09d8c993b2e19af839899f07
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-16 18:09:40 +0700

    feat: implement `doGet` method in `CurrencyServlet`

--- src/main/java/pet/project/servlet/currency/CurrencyServlet.java
+++ src/main/java/pet/project/servlet/currency/CurrencyServlet.java
@@ -1,17 +1,30 @@
 package pet.project.servlet.currency;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pet.project.Utils;
+import pet.project.model.Currency;
+import pet.project.repository.CurrencyRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
-@WebServlet(name = "CurrencyServlet", urlPatterns = "/currency/{param}")
+@WebServlet(name = "CurrencyServlet", urlPatterns = "/currency/*")
 public class CurrencyServlet extends HttpServlet {
+    private final CurrencyRepository currencyRepository = new CurrencyRepository(Utils.getConfiguredDataSource());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        // TODO: Validation of path
+        String code = req.getPathInfo().replaceAll("/", "").toUpperCase();
+        // TODO: Check Optional
+        Optional<Currency> currency = currencyRepository.getByCode(code);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(resp.getWriter(), currency.get());
     }
 }
