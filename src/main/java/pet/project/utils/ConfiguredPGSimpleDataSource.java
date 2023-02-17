2ca230911689e9b0171ac8a56b44ea01b1c1821d
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-17 18:46:09 +0700

    feat: add ConfiguredPGSimpleDataSource class, that serves as kind of Singleton

---
+++ src/main/java/pet/project/utils/ConfiguredPGSimpleDataSource.java
@@ -0,0 +1,33 @@
+package pet.project.utils;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ConfiguredPGSimpleDataSource {
+    private static final PGSimpleDataSource INSTANCE = new PGSimpleDataSource();
+
+    static {
+        try (InputStream input = new FileInputStream("src/main/resources/db.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            INSTANCE.setURL(prop.getProperty("db.url"));
+            INSTANCE.setUser(prop.getProperty("db.username"));
+            INSTANCE.setPassword(prop.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ConfiguredPGSimpleDataSource() {
+    }
+
+    public static PGSimpleDataSource getInstance() {
+        return INSTANCE;
+    }
+}
