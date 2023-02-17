970a47acf874ededab795c326cf0fdfe72d4d297
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-17 18:56:26 +0700

    feat: implement doGet method in ExchangeRatesServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
@@ -1,18 +1,27 @@
 package pet.project.servlet.exchange;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pet.project.model.ExchangeRate;
+import pet.project.repository.ExchangeRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ExchangeRatesServlet", urlPatterns = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
+    private final ExchangeRepository exchangeRepository = new ExchangeRepository();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<ExchangeRate> exchangeRates = exchangeRepository.findAll();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(resp.getWriter(), exchangeRates);
     }
 
     @Override
