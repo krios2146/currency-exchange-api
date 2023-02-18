efc93825889a2eb92ababdd2065f614ea1ced241
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-18 13:31:38 +0700

    feat: implement doPost method in ExchangeRatesServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
@@ -2,6 +2,7 @@ package pet.project.servlet.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pet.project.model.ExchangeRate;
+import pet.project.repository.CurrencyRepository;
 import pet.project.repository.ExchangeRepository;
 
 import javax.servlet.ServletException;
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet(name = "ExchangeRatesServlet", urlPatterns = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
     private final ExchangeRepository exchangeRepository = new ExchangeRepository();
+    private final CurrencyRepository currencyRepository = new CurrencyRepository();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,6 +29,22 @@ public class ExchangeRatesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        // TODO: Validation of request
+        String baseCurrencyCode = req.getParameter("from");
+        String targetCurrencyCode = req.getParameter("to");
+        String rate = req.getParameter("rate");
+
+        // TODO: Check if this rate is already present
+        ExchangeRate exchangeRate = new ExchangeRate(
+                currencyRepository.findByCode(baseCurrencyCode).get(),
+                currencyRepository.findByCode(targetCurrencyCode).get(),
+                Double.parseDouble(rate)
+        );
+
+        exchangeRepository.save(exchangeRate);
+        Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(resp.getWriter(), exchangeRateOptional.get());
     }
 }
