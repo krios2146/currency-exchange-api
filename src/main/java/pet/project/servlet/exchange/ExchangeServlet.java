ec912779be16d66a1d9075abc74df1185ed0667d
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-18 15:56:30 +0700

    feat: add validation to parameters in ExchangeServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeServlet.java
@@ -19,21 +19,31 @@ public class ExchangeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO: Validate parameters
-        String from = req.getParameter("from");
-        String to = req.getParameter("to");
-        double amount = Double.parseDouble(req.getParameter("amount"));
+        String baseCurrencyCode = req.getParameter("from");
+        String targetCurrencyCode = req.getParameter("to");
+        String amount = req.getParameter("amount");
+
+        if (baseCurrencyCode == null || targetCurrencyCode == null || amount == null ||
+                baseCurrencyCode.isBlank() || targetCurrencyCode.isBlank() || amount.isBlank()) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameters: from, to or amount");
+            return;
+        }
+
+        if (baseCurrencyCode.length() != 3 || targetCurrencyCode.length() != 3) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency codes must be in ISO 4217 format");
+            return;
+        }
 
         // TODO: Check Optional
-        Optional<ExchangeRate> exchangeRate = exchangeRepository.findByCodes(from, to);
+        Optional<ExchangeRate> exchangeRate = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
         double currencyExchangeRate = exchangeRate.get().getRate();
-        double convertedAmount = amount * currencyExchangeRate;
+        double convertedAmount = Double.parseDouble(amount) * currencyExchangeRate;
 
         ExchangeResponse response = new ExchangeResponse(
                 exchangeRate.get().getBaseCurrency(),
                 exchangeRate.get().getTargetCurrency(),
                 currencyExchangeRate,
-                amount,
+                Double.parseDouble(amount),
                 convertedAmount
         );
 
