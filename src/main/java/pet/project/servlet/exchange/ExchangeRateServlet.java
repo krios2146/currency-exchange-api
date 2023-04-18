91d5503badc45b3843bff80c51f035a5ffe92d70
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-18 20:10:38 +0700

    refactor(servlet): add catch blocks to the ExchangeRateServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeRateServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeRateServlet.java
@@ -2,6 +2,7 @@ package pet.project.servlet.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pet.project.model.ExchangeRate;
+import pet.project.model.response.ErrorResponse;
 import pet.project.repository.ExchangeRepository;
 import pet.project.repository.JdbcExchangeRepository;
 
@@ -12,7 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.Optional;
+
+import static pet.project.utils.Validation.isValidCurrencyCode;
 
 @WebServlet(name = "ExchangeRateServlet", urlPatterns = "/exchangeRate/*")
 public class ExchangeRateServlet extends HttpServlet {
@@ -29,59 +33,131 @@ public class ExchangeRateServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String url = req.getPathInfo().replaceAll("/", "");
 
         if (url.length() != 6) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                    "Currency codes are either not provided or provided in an incorrect format");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Currency codes are either not provided or provided in an incorrect format"
+            ));
             return;
         }
 
-        String baseCurrencyCode = url.substring(0, 3).toUpperCase();
-        String targetCurrencyCode = url.substring(3).toUpperCase();
+        String baseCurrencyCode = url.substring(0, 3);
+        String targetCurrencyCode = url.substring(3);
 
-        Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
-        if (exchangeRateOptional.isEmpty()) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "There is no exchange rate for this currency pair");
+        if (!isValidCurrencyCode(baseCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Base currency code must be in ISO 4217 format"
+            ));
             return;
         }
 
-        objectMapper.writeValue(resp.getWriter(), exchangeRateOptional.get());
+        if (!isValidCurrencyCode(targetCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Target currency code must be in ISO 4217 format"
+            ));
+            return;
+        }
+
+        try {
+            Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
+
+            if (exchangeRateOptional.isEmpty()) {
+                objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                        HttpServletResponse.SC_NOT_FOUND,
+                        "There is no exchange rate for this currency pair"
+                ));
+                return;
+            }
+
+            objectMapper.writeValue(resp.getWriter(), exchangeRateOptional.get());
+
+        } catch (SQLException e) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Something happened with the database, try again later!"
+            ));
+        }
     }
 
     protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String url = req.getPathInfo().replaceAll("/", "");
-        // TODO: Proper validation
+
         if (url.length() != 6) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                    "Currency codes are either not provided or provided in an incorrect format");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Currency codes are either not provided or provided in an incorrect format"
+            ));
             return;
         }
 
         String parameter = req.getReader().readLine();
         if (parameter == null || !parameter.contains("rate")) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required parameter rate");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Missing required parameter rate"
+            ));
             return;
         }
 
-        String baseCurrencyCode = url.substring(0, 3).toUpperCase();
-        String targetCurrencyCode = url.substring(3).toUpperCase();
+        String baseCurrencyCode = url.substring(0, 3);
+        String targetCurrencyCode = url.substring(3);
         String paramRateValue = parameter.replace("rate=", "");
 
-        Optional<ExchangeRate> exchangeRateToUpdateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
-        if (exchangeRateToUpdateOptional.isEmpty()) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "There is no exchange rate for this currency pair");
+        if (!isValidCurrencyCode(baseCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Base currency code must be in ISO 4217 format"
+            ));
             return;
         }
 
-        ExchangeRate exchangeRateToUpdate = exchangeRateToUpdateOptional.get();
+        if (!isValidCurrencyCode(targetCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Target currency code must be in ISO 4217 format"
+            ));
+            return;
+        }
+
+        BigDecimal rate;
         try {
-            exchangeRateToUpdate.setRate(BigDecimal.valueOf(Double.parseDouble(paramRateValue)));
-            exchangeRepository.update(exchangeRateToUpdate);
-            doGet(req, resp);
+            rate = BigDecimal.valueOf(Double.parseDouble(paramRateValue));
         } catch (NumberFormatException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Incorrect value of rate parameter");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Incorrect value of rate parameter"
+            ));
+            return;
+        }
+
+        try {
+
+            Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
+
+            if (exchangeRateOptional.isEmpty()) {
+                objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                        HttpServletResponse.SC_NOT_FOUND,
+                        "There is no exchange rate for this currency pair"
+                ));
+                return;
+            }
+
+            ExchangeRate exchangeRate = exchangeRateOptional.get();
+            exchangeRate.setRate(rate);
+            exchangeRepository.update(exchangeRate);
+
+            objectMapper.writeValue(resp.getWriter(), exchangeRate);
+
+        } catch (SQLException e) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Something happened with the database, try again later!"
+            ));
         }
     }
 }
