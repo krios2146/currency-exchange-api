f72d7168f83dad6a4477e31e308fb627557fa7db
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-19 15:06:53 +0700

    refactor(servlet): add catch blocks to the ExchangeRatesServlet

--- src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
+++ src/main/java/pet/project/servlet/exchange/ExchangeRatesServlet.java
@@ -2,77 +2,106 @@ package pet.project.servlet.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pet.project.model.ExchangeRate;
+import pet.project.model.response.ErrorResponse;
 import pet.project.repository.CurrencyRepository;
 import pet.project.repository.ExchangeRepository;
 import pet.project.repository.JdbcCurrencyRepository;
 import pet.project.repository.JdbcExchangeRepository;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
+
+import static pet.project.utils.Validation.isValidCurrencyCode;
 
 @WebServlet(name = "ExchangeRatesServlet", urlPatterns = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
     private final ExchangeRepository exchangeRepository = new JdbcExchangeRepository();
     private final CurrencyRepository currencyRepository = new JdbcCurrencyRepository();
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String INTEGRITY_CONSTRAINT_VIOLATION_CODE = "23505";
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<ExchangeRate> exchangeRates = exchangeRepository.findAll();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            List<ExchangeRate> exchangeRates = exchangeRepository.findAll();
+            objectMapper.writeValue(resp.getWriter(), exchangeRates);
 
-        objectMapper.writeValue(resp.getWriter(), exchangeRates);
+        } catch (SQLException e) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Something happened with the database, try again later!"
+            ));
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String baseCurrencyCode = req.getParameter("baseCurrencyCode");
         String targetCurrencyCode = req.getParameter("targetCurrencyCode");
-        String rate = req.getParameter("rate");
+        String rateParam = req.getParameter("rate");
 
-        if (baseCurrencyCode == null || targetCurrencyCode == null || rate == null ||
-                baseCurrencyCode.isBlank() || targetCurrencyCode.isBlank() || rate.isBlank()) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameters: baseCurrencyCode, targetCurrencyCode or rate");
+        if (!isValidCurrencyCode(baseCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Base currency code must be in ISO 4217 format"
+            ));
             return;
         }
 
-        if (baseCurrencyCode.length() != 3 || targetCurrencyCode.length() != 3) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency codes must be in ISO 4217 format");
+        if (!isValidCurrencyCode(targetCurrencyCode)) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Target currency code must be in ISO 4217 format"
+            ));
             return;
         }
 
-        // Is this controller responsibility to check if entry already exists?
-        Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
-        if (exchangeRateOptional.isPresent()) {
-            resp.sendError(HttpServletResponse.SC_CONFLICT,
-                    "The exchange rate you are trying to add already exists, id = " + exchangeRateOptional.get().getId());
+        BigDecimal rate;
+        try {
+            rate = BigDecimal.valueOf(Double.parseDouble(rateParam));
+        } catch (NumberFormatException e) {
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Incorrect value of rate parameter"
+            ));
             return;
         }
 
         try {
-            ExchangeRate exchangeRateToAdd = new ExchangeRate(
-                    currencyRepository.findByCode(baseCurrencyCode).get(),
-                    currencyRepository.findByCode(targetCurrencyCode).get(),
-                    BigDecimal.valueOf(Double.parseDouble(rate))
+            ExchangeRate exchangeRate = new ExchangeRate(
+                    currencyRepository.findByCode(baseCurrencyCode).orElseThrow(),
+                    currencyRepository.findByCode(targetCurrencyCode).orElseThrow(),
+                    rate
             );
 
-            exchangeRepository.save(exchangeRateToAdd);
-            ExchangeRate addedExchangeRate = exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode).get();
-            
-            objectMapper.writeValue(resp.getWriter(), addedExchangeRate);
+            Long savedExchangeRateId = exchangeRepository.save(exchangeRate);
+            exchangeRate.setId(savedExchangeRateId);
 
-        } catch (NumberFormatException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Incorrect value of rate parameter");
+            objectMapper.writeValue(resp.getWriter(), exchangeRate);
+
         } catch (NoSuchElementException e) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND,
-                    "One or both currencies for which you are trying to add an exchange rate does not exist in the database");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_NOT_FOUND,
+                    "One or both currencies for which you are trying to add an exchange rate does not exist in the database"
+            ));
+        } catch (SQLException e) {
+            if (e.getSQLState().equals(INTEGRITY_CONSTRAINT_VIOLATION_CODE)) {
+                objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                        HttpServletResponse.SC_CONFLICT,
+                        e.getMessage()
+                ));
+            }
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Something happened with the database, try again later!"
+            ));
         }
     }
 }
