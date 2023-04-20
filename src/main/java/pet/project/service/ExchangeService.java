cf1a00bef246bcd2a01b47d5bed2b142a548b402
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-20 15:20:40 +0700

    feat(service): add javadoc for all methods in the ExchangeService

--- src/main/java/pet/project/service/ExchangeService.java
+++ src/main/java/pet/project/service/ExchangeService.java
@@ -8,6 +8,7 @@ import pet.project.repository.JdbcExchangeRepository;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static java.math.MathContext.DECIMAL64;
@@ -15,8 +16,20 @@ import static java.math.MathContext.DECIMAL64;
 public class ExchangeService {
     private final ExchangeRepository exchangeRepository = new JdbcExchangeRepository();
 
+    /**
+     * Converts the given amount from one currency to another using the exchange rate
+     * retrieved from a data source.
+     *
+     * @param baseCurrencyCode   The currency code of the base currency to convert from.
+     * @param targetCurrencyCode The currency code of the target currency to convert to.
+     * @param amount             The amount to be converted.
+     * @return An {@link ExchangeResponse} object containing the conversion details, such as the
+     * base currency, target currency, exchange rate, original amount, and converted amount.
+     * @throws SQLException           If there is an error accessing the data source.
+     * @throws NoSuchElementException If no exchange rate is found for the given currency codes.
+     */
     // probably mixing logic and view
-    public ExchangeResponse convertCurrency(String baseCurrencyCode, String targetCurrencyCode, BigDecimal amount) throws SQLException {
+    public ExchangeResponse convertCurrency(String baseCurrencyCode, String targetCurrencyCode, BigDecimal amount) throws SQLException, NoSuchElementException {
         ExchangeRate exchangeRate = getExchangeRate(baseCurrencyCode, targetCurrencyCode).orElseThrow();
 
         BigDecimal convertedAmount = amount.multiply(exchangeRate.getRate());
@@ -29,7 +42,18 @@ public class ExchangeService {
                 convertedAmount
         );
     }
-    
+
+    /**
+     * Retrieves the exchange rate between two currencies.
+     * <p>
+     * The method first tries to fetch the direct exchange rate, if not found,
+     * then it tries reverse exchange rate, and finally cross exchange rate.
+     *
+     * @param baseCurrencyCode   The currency code of the base currency.
+     * @param targetCurrencyCode The currency code of the target currency.
+     * @return An Optional of  {@link ExchangeRate} object containing the exchange rate if found, or empty if not found.
+     * @throws SQLException If there is an error accessing the data source.
+     */
     private Optional<ExchangeRate> getExchangeRate(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
         Optional<ExchangeRate> exchangeRateOptional = getDirectExchangeRate(baseCurrencyCode, targetCurrencyCode);
 
@@ -44,10 +68,29 @@ public class ExchangeService {
         return exchangeRateOptional;
     }
 
+    /**
+     * Retrieves the exchange rate between two currencies directly from the exchange repository,
+     * based on the given base currency and target currency codes.
+     *
+     * @param baseCurrencyCode   The currency code of the base currency.
+     * @param targetCurrencyCode The currency code of the target currency.
+     * @return An Optional of {@link ExchangeRate} object containing the exchange rate if found, or empty if not found.
+     * @throws SQLException If there is an error accessing the data source.
+     */
     private Optional<ExchangeRate> getDirectExchangeRate(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
         return exchangeRepository.findByCodes(baseCurrencyCode, targetCurrencyCode);
     }
 
+    /**
+     * Retrieves the reversed exchange rate between two currencies from the exchange repository,
+     * based on the given base currency and target currency codes, and returns the corresponding
+     * direct exchange rate.
+     *
+     * @param baseCurrencyCode   The currency code of the base currency.
+     * @param targetCurrencyCode The currency code of the target currency.
+     * @return An Optional of {@link ExchangeRate} object containing the reverse exchange rate if found, or empty if not found.
+     * @throws SQLException If there is an error accessing the data source.
+     */
     private Optional<ExchangeRate> getReverseExchangeRate(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
         Optional<ExchangeRate> exchangeRateOptional = exchangeRepository.findByCodes(targetCurrencyCode, baseCurrencyCode);
 
@@ -66,6 +109,17 @@ public class ExchangeService {
         return Optional.of(directExchangeRate);
     }
 
+    /**
+     * Retrieves the cross exchange rate between two currencies from the exchange repository,
+     * based on the given base currency and target currency codes. This method searches for
+     * the USD to base currency and USD to target currency rates, and calculates the corresponding
+     * base currency to target currency exchange rate.
+     *
+     * @param baseCurrencyCode   The currency code of the base currency.
+     * @param targetCurrencyCode The currency code of the target currency.
+     * @return An Optional of {@link ExchangeRate} object containing the cross exchange rate if found, or empty if not found.
+     * @throws SQLException If there is an error accessing the data source.
+     */
     private Optional<ExchangeRate> getCrossExchangeRate(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
         List<ExchangeRate> ratesWithUsdBase = exchangeRepository.findByCodesWithUsdBase(baseCurrencyCode, targetCurrencyCode);
 
