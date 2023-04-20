6692cae4c274ba4f1a0a8f12c45e61184d8d03d7
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-20 18:31:01 +0700

    feat: ConfiguredDataSource now prefer system env variables for configuring over .properties file

--- src/main/java/pet/project/utils/ConfiguredDataSource.java
+++ src/main/java/pet/project/utils/ConfiguredDataSource.java
@@ -2,7 +2,6 @@ package pet.project.utils;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -12,16 +11,26 @@ public class ConfiguredDataSource {
     private static final PGSimpleDataSource INSTANCE = new PGSimpleDataSource();
 
     static {
-        try (InputStream input = new FileInputStream("src/main/resources/db.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
+        String url = System.getenv("DB_URL");
+        String username = System.getenv("DB_USERNAME");
+        String password = System.getenv("DB_PASSWORD");
 
-            INSTANCE.setURL(prop.getProperty("db.url"));
-            INSTANCE.setUser(prop.getProperty("db.username"));
-            INSTANCE.setPassword(prop.getProperty("db.password"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if (url == null || username == null || password == null) {
+            try (InputStream input = ConfiguredDataSource.class.getResourceAsStream("/db.properties")) {
+                Properties prop = new Properties();
+                prop.load(input);
+
+                url = prop.getProperty("db.url");
+                username = prop.getProperty("db.username");
+                password = prop.getProperty("db.password");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+        INSTANCE.setURL(url);
+        INSTANCE.setUser(username);
+        INSTANCE.setPassword(password);
     }
 
     private ConfiguredDataSource() {
